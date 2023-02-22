478ae5155dfda3d6fadfcf00663b141958f77c3a
Mike Cecconello <mikececco2000@gmail.com>
2023-02-22 14:45:46 +0100

    Added index and link to single restaurant

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  VALID_VALUES = (0..5)
-
+  
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
