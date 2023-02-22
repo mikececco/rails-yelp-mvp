f20581a30966b7e6d9b769dd63f9e38cc69411e0
Mike Cecconello <mikececco2000@gmail.com>
2023-02-22 16:47:10 +0100

    Reivews displayed for each restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,9 +3,11 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
+
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
